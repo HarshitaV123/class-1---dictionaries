e244c1acd78d0abd1f92b1da34f5ede62146a7df
HarshitaV123 <100452399+HarshitaV123@users.noreply.github.com>
2024-10-19 12:56:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+## creating a dictionary 
+states= {"chicago":"springfield","florida":"tallahase","indiana":"indianapolis","texas":"austin"}
+print(states)
+## accesing values from dictionary
+my_state=states["indiana"]
+print(f"I live in {my_state}")
+## creating a list
+flowers=["roses","daises","tulips","sunflowers"]
+fav_flower=flowers[2]
+print(f"My favorite flowers are {fav_flower}")
+## getting the list of keys
+key_list=list(states.keys())
+print(key_list)
